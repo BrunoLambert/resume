f00bbe8a7e179abecf5e830b7dff812ede68e9f7
Bruno Lambert <brunofsclambert@gmail.com>
2019-12-28 21:58:15 -0300

    Now deploy settings

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,5 +1,3 @@
-import colors from 'vuetify/es5/util/colors'
-
 export default {
   mode: 'spa',
   /*
@@ -55,13 +53,13 @@ export default {
       light: true,
       themes: {
         light: {
-          primary: colors.blue.darken2,
-          accent: colors.grey.darken3,
-          secondary: colors.amber.darken3,
-          info: colors.teal.lighten1,
-          warning: colors.amber.base,
-          error: colors.deepOrange.accent4,
-          success: colors.green.accent3
+          primary: '#FFF',
+          accent: '#FFF',
+          secondary: '#FFF',
+          info: '#FFF',
+          warning: '#FFF',
+          error: '#FFF',
+          success: '#FFF'
         }
       }
     }
@@ -75,5 +73,8 @@ export default {
     */
     extend (config, ctx) {
     }
+  },
+  generate: {
+    dir: "public"
   }
 }
